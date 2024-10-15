70e2b0647a6a174ca900fa4a4cb5cb0aa759336a
ms20002000 <shojaeim2000@gmail.com>
2024-10-15 17:00:24 +0330

    account view

--- account/urls.py
+++ account/urls.py
@@ -2,13 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('register/', register, name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(next_page=reverse_lazy('list_user')), name='logout'),
-    # path('login_first/', login_first, name='login_first'),
-    # path('logout/', logout, name='logout'),
-    # path('dashboard/', dashboard, name='dashboard'),
-    path('register/', RegularUserCreateView.as_view(), name='rigister'),
+    path('logout/', logout, name='logout'),
+    path('register/', RegularUserCreateView.as_view(), name='register'),
     path('list_user/', RegularUserListView.as_view(), name='list_user'),
-    
 ]
--- account/views.py
+++ account/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
-from django.http.response import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .forms import UserRegistrationForm
-from django.contrib.auth.models import User
-
-
+from django.contrib.auth import logout as dj_logout
+from django.utils.decorators import method_decorator
+from django.http import HttpResponseRedirect
 from django.views.generic import CreateView, ListView
 from .models import RegularUser
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
+from django import forms
+
 
 class RegularUserCreateView(CreateView):
     model = RegularUser
-    fields = ['email', 'first_name', 'last_name', 'phone_number', 'royalty_points', 'password']
+    fields = ['email', 'first_name', 'last_name', 'phone_number', 'password']
     template_name = 'account/register.html'
     success_url = reverse_lazy('list_user') 
+
+    def get_form_class(self):
+        form_class = super().get_form_class()
+        form_class.base_fields['password'].widget = forms.PasswordInput()  
+        return form_class
 
     def form_valid(self, form):
         user = form.save(commit=False)
@@ -25,6 +27,7 @@ class RegularUserCreateView(CreateView):
         user.save()
         return super().form_valid(form)
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class RegularUserListView(ListView):
     model = RegularUser
     template_name = 'account/list_users.html'
@@ -38,70 +41,9 @@ class UserLoginView(LoginView):
         return self.success_url
 
 
-class UserLogoutView(LogoutView):
-    next_page = reverse_lazy('list_user')
-
-# def register(request):
-#     if request.method == 'GET':
-#         form = UserRegistrationForm(request.POST)
-#         return render(request, 'account/register.html', {'form': form})
-#     elif request.method == 'POST':
-#         form = UserRegistrationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             dj_login(request, user)
-#             # return render(request, 'account/dashboard.html', {'user': user})
-#             return render(request, 'account/list_users.html', {'users': User.objects.all()})
-#         else:
-#             return render(request, 'account/register.html', {'form': form})
-#     else:
-#         return HttpResponse('Only get/post method allowed')
-
-
-def login_first(request):
-    return HttpResponse('Please login first')
-
-
-# def login(request):
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         password = request.POST['password']
-
-#         user = authenticate(request, username=username, password=password)
-#         if user:
-#             dj_login(request, user)
-#             return render(request, 'account/list_users.html', {'users': User.objects.all()})
-#         return HttpResponse('Wrong password/username')
-
-#     elif request.method == 'GET':
-#         form = AuthenticationForm()
-#         return render(request, 'account/login.html', {'form': form})
-
-#     return HttpResponse('Please login with post method')
-
-
-# def login(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             user = form.get_user()
-#             return render(request, 'account/dashboard.html', {'user': user})
-#     else:
-#         form = LoginForm()
-#     return render(request, 'account/login.html', {'form': form})
+def logout(request):
+    if request.method == 'POST':
+        dj_logout(request)
+        return HttpResponseRedirect(reverse_lazy('register'))
     
-
-# @login_required(login_url='/account/login_first/')
-# def logout(request):
-#     if request.method == 'POST':
-#         if not request.user.is_authenticated:
-#             return HttpResponse('Please login first')
-
-#         dj_logout(request)
-#         return HttpResponse('Logout successfully')
-
-#     return HttpResponse('Only post method allowed')
-
-# @login_required(login_url='/account/login_first/')
-# def dashboard(request):
-#     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    return render(request, 'account/logout.html', {})
