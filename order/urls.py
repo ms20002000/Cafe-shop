c72aaae6e890fe7dd20d4363f7a63bc61ff71c37
ms20002000 <shojaeim2000@gmail.com>
2024-10-21 09:48:33 +0330

    order urls

---
+++ order/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('create/', OrderCreateView.as_view(), name='order_create'),
+    path('update/<int:pk>/', OrderUpdateView.as_view(), name='order_update'),
+    path('delete/<int:pk>/', OrderDeleteView.as_view(), name='order_delete'),
+]
