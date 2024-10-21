fe11fb5f4f1300acf7f442eda40ef9ed55713afd
ms20002000 <shojaeim2000@gmail.com>
2024-10-21 10:33:59 +0330

    order migration

--- order/models.py
+++ order/models.py
@@ -13,9 +13,9 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=[('P', 'Pending'), ('C', 'Completed')])
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    modify_by = models.OneToOneField(CustomUser, default='Customer', on_delete=models.CASCADE)
-    payment_method = models.CharField(default='Cash')
-    table = models.OneToOneField(Table, on_delete=models.CASCADE, related_name='order')
+    payment_method = models.CharField(max_length=20, default='Cash')
+    table = models.OneToOneField(Table, on_delete=models.CASCADE, related_name='order', null=True, blank=True)
+    modify_by = models.OneToOneField(CustomUser, default=1, on_delete=models.CASCADE)
 
     def total_items_count(self):
         total = 0
@@ -34,7 +34,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items', default=1)
     quantity = models.PositiveIntegerField(default=1)
 
     def total_price(self):
