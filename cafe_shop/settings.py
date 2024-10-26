a104b07f287911776bf9f8f65473166b3299778b
ms20002000 <shojaeim2000@gmail.com>
2024-10-26 09:39:54 +0330

    staff can not open admin panel with middleware

--- cafe_shop/settings.py
+++ cafe_shop/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'account.middleware.RestrictStaffAdminAccessMiddleware',
 ]
 
 ROOT_URLCONF = 'cafe_shop.urls'
