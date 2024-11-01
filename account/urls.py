ec9b4c4349f192816671c4387a35be911e474556
Faraz Fallah <farazfallah0@gmail.com>
2024-11-01 14:59:16 +0330

    Your commit message

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', HomeView, name='home'),
+    # path('', HomeView, name='home'),
     path('login/', StaffLogin.as_view(), name='staff_login'),
     path('manager_dashboard/', ManagerPanelView.as_view(), name='manager_dashboard'),
     path('staff_dashboard/', StaffDashboard.as_view(), name='staff_dashboard'),
